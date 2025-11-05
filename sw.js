b9a0777705b75f02517c929f87f9d8b3c1e38f99
dlthoibinh <lamdltb@gmail.com>
2025-11-05 09:26:04 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
-const CACHE_NAME = 'hyperrush-v1';
+// HyperRush Freeze SW - v3
+const CACHE_NAME = 'hr-freeze-v3';
 const ASSETS = [
-  './', './index.html', './manifest.json',
-  './icon-192.png', './icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
-  );
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  if (e.request.method !== 'GET') return;
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
-        return resp;
-      }))
-    );
-  }
+self.addEventListener('fetch', (e)=>{
+  const req = e.request;
+  if (req.method !== 'GET') return; // để POST tới GAS đi thẳng mạng
+  e.respondWith(
+    caches.match(req).then(res=>res || fetch(req).then(r=>{
+      const copy = r.clone();
+      if (r.ok && new URL(req.url).origin === location.origin) {
+        caches.open(CACHE_NAME).then(c=>c.put(req, copy));
+      }
+      return r;
+    }).catch(()=>caches.match('./')))
+  );
 });
